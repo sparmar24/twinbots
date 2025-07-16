b74bde348d45cd7ae8e23b0c4336bd50aabbb1a9
Sonia Parmar <soniaparmar24@gmail.com>
2025-07-16 20:32:17 +0200

    feat(settings): switch to PostgreSQL and use double quotes

--- web-apps/coresite/settings.py
+++ web-apps/coresite/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = "coresite.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "mydb",
+        "USER": "myuser",
+        "PASSWORD": "mypassword",
+        "HOST": "db",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
